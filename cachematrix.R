f6e376bf387b702c9f4e40fd92d3273272f44e5e
Taco Trimbach <tacotrimbach@gmail.com>
2018-07-05 22:36:30 +0200

    Prog. Ass. 2 C2W3

--- cachematrix.R
+++ cachematrix.R
@@ -30,7 +30,7 @@ makeCacheMatrix <- function(x = matrix()) {
              setinverse = setinverse,
              getinverse = getinverse)
 }
- 
+
 
 ## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above.
 ## If the inverse has already been calculated (and the matrix has not changed),
